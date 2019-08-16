b97aa2a1a79517c22c2a40a222aa2edd12a3dc70
caringpeople1 <54032408+caringpeople1@users.noreply.github.com>
2019-08-16 15:37:15 -0400

    Add files via upload

--- slide.js
+++ slide.js
@@ -1,41 +1,70 @@
-var popupWindow=null;
-var mypage=tips.HTML
-var new_tip;
-var randomNumber = Math.floor(Math.random() * tips.length);
-var randomTip = tips [randomNumber]
+var place;
+var Window=null;
 
-var tips = [
-  'I really love apples',
-  'I really hate oranges',
-  'I think this is amazing',
-  'Hello World'
-];
-console.log() 
-/*function new_tip(){
-  var random_tip = Math.floor (Math.random() * (list.length));
-  document.getElementById("list").innerHTML.tip.content
-   list[1]*/
-
-
-function popup(w,h,pos,infocus){
-  if (pos == 'random') {
-    LeftPosition=(screen.width)?Math.floor(Math.random()*(screen.width-w)):100;TopPosition=(screen.height)?Math.floor(Math.random()*((screen.height-h)-75)):100;
+function update_place(loc) {
+   if (loc == "Home") {
+     place = "Home";
+   }
+  if (loc == "School") {
+    place = "School";
   }
-  else {
-    LeftPosition=(screen.width)?(screen.width-w)/2:100;TopPosition=(screen.height)?(screen.height-h)/2:100;
+  if (loc == "Work") {
+    place ="Work";
   }
-  settings = 'width='+ w + ',height='+ h + ',top=' + TopPosition + ',left=' + LeftPosition + ',scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no';
-  popupWindow = window.open(mypage,'',settings);
-  if (infocus=='front') {
-    popupWindow.focus();
-    popupWindow.location=mypage;
-  }
-  if (infocus=='back') {
-    popupWindow.blur();
-    popupWindow.location = mypage;
-    popupWindow.blur();
+  if (loc == "Public Place") {
+      place = "Public Place";
   }
 }
 
-//if user_input == button5 {
-  //show(file:///C:/Users/GWC/Desktop/Tip1.html')}
+function open_tip() {
+  var slider = document.getElementById("myRange");
+  var stresslevel = slider.value;
+  console.log("stresslevel: ", stresslevel);
+  console.log("place: ", place);
+  if (place == "Home" && (stresslevel == 0 || stresslevel == 1)) {
+    popup('C:/Users/GWC/Desktop/Tip1.html','tip','640','480','center','front')
+  }
+  if (place == "Home"&& (stresslevel == 2 || stresslevel == 3)) {
+    popup('C:/Users/GWC/Desktop/Tip2.html','tip','640','480','center','front')
+  }
+  if (place == "Home" && (stresslevel == 4 || stresslevel == 5)) {
+    popup('C:/Users/GWC/Desktop/Tip3.html','tip','640','480','center','front')
+  }
+  if (place == "School" && (stresslevel == 0 || stresslevel == 1)) {
+    popup('C:/Users/GWC/Desktop/Tip4.html','tip','640','480','center','front')
+  }
+  if (place == "School" && (stresslevel == 2 || stresslevel == 3)) {
+    popup('C:/Users/GWC/Desktop/Tip5.html','tip','640','480','center','front')
+  }
+  if (place == "School" && (stresslevel == 4 || stresslevel == 5))  {
+    popup('C:/Users/GWC/Desktop/Tip6.html','tip','640','480','center','front')
+  }
+  if (place == "Work"  && (stresslevel == 0 || stresslevel == 1)) {
+    popup('C:/Users/GWC/Desktop/Tip7.html','tip','640','480','center','front')
+  }
+  if (place == "Work" && (stresslevel == 2 || stresslevel == 3)) {
+    popup('C:/Users/GWC/Desktop/Tip8.html','tip','640','480','center','front')
+  }
+  if (place == "Work" && (stresslevel == 4 || stresslevel == 5)) {
+    popup('C:/Users/GWC/Desktop/Tip9.html','tip','640','480','center','front')
+  }
+  if (place == "Public Place" && (stresslevel == 0 || stresslevel == 1)) {
+    popup('C:/Users/GWC/Desktop/Tip10.html','tip','640','480','center','front')
+  }
+  if (place == "Public Place" && (stresslevel == 2 || stresslevel == 3)) {
+    popup('C:/Users/GWC/Desktop/Tip11.html','tip','640','480','center','front')
+  }
+  if (place == "Public Place" && (stresslevel == 4 || stresslevel == 5)) {
+    popup('C:/Users/GWC/Desktop/Tip12.html','tip','640','480','center','front')
+  }
+}
+
+function popup(mypage,myname,w,h,pos,infocus){
+  if (pos == 'random')
+{LeftPosition=(screen.width)?Math.floor(Math.random()*(screen.width-w)):100;TopPosition=(screen.height)?Math.floor(Math.random()*((screen.height-h)-75)):100;}
+  else
+{LeftPosition=(screen.width)?(screen.width-w)/2:100;TopPosition=(screen.height)?(screen.height-h)/2:100;}
+settings='width='+ w + ',height='+ h + ',top=' + TopPosition + ',left=' + LeftPosition + ',scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no';Window=window.open('',myname,settings);
+  if(infocus=='front'){Window.focus();Window.location=mypage;}
+  if(infocus=='back'){Window.blur();Window.location=mypage;Window.blur();}
+}
